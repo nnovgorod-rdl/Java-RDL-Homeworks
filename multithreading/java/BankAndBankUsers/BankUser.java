ba40532d2e05632caaed8e0af3a6539fec846702
D Demidov <dmitr.demidov2018@yandex.ru>
2020-03-18 21:26:29 +0300

    Multithreading

---
+++ multithreading/java/BankAndBankUsers/BankUser.java
@@ -0,0 +1,35 @@
+package BankAndBankUsers;
+
+class BankUser extends Thread {
+    String name;
+    Bank bank;
+
+    BankUser(Bank bank, String name) {
+        this.bank = bank;
+        this.name = name;
+    }
+    public void makeTransfer(int moneToTransfer) throws NotEnoughMoneyException {
+        synchronized (bank){
+            if (bank.hasEnoughMoney(moneToTransfer)) {
+                this.bank.transferMoney(moneToTransfer);
+            } else {
+                throw new NotEnoughMoneyException("Денег нет!");
+            }}
+    }
+
+
+    @Override
+    public void run()
+    {
+        while (true) {
+            try {
+                System.out.println(this.name);
+                this.makeTransfer(5);
+            } catch (NotEnoughMoneyException e) {
+                System.out.println("Деньги кончились");
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+}
