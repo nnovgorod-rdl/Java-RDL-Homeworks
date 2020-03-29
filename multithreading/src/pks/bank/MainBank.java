271d1c6419d88d4f356dbeee782d6c2d2f6fd1cd
Kirill <kirill_nn@inbox.ru>
2020-03-29 23:13:31 +0300

    Answer

--- multithreading/src/pks/bank/MainBank.java
+++ multithreading/src/pks/bank/MainBank.java
@@ -3,62 +3,29 @@ package pks.bank;
 import pks.bank.work.Bank;
 import pks.bank.work.BankUser;
 
+import java.util.ArrayList;
+
 public class MainBank {
-    static BankUser bankUserFirst;
-    static BankUser bankUserSecond;
-    static BankUser bankUserThird;
-    static BankUser bankUserFourth;
-    static BankUser bankUserFifth;
-    static BankUser bankUserSixth;
-    static BankUser bankUserSeventh;
-    static BankUser bankUserEighth;
-    static BankUser bankUserNinth;
-    static BankUser bankUserTenth;
+    static ArrayList<BankUser> bankUserArrayList = new ArrayList<>();
+    static ArrayList<Thread> threadsArrayList = new ArrayList<>();
+    static final int MAX_BANK_USERS = 10;
 
     public static void main(String[] args) {
         Bank bank = new Bank(1000);
         System.out.println(bank.getMoneyAmount());
 
-        bankUserFirst = new BankUser(bank, "01");
-        bankUserSecond = new BankUser(bank, "02");
-        bankUserThird = new BankUser(bank, "03");
-        bankUserFourth = new BankUser(bank, "04");
-        bankUserFifth = new BankUser(bank, "05");
-        bankUserSixth = new BankUser(bank, "06");
-        bankUserSeventh = new BankUser(bank, "07");
-        bankUserEighth = new BankUser(bank, "08");
-        bankUserNinth = new BankUser(bank, "09");
-        bankUserTenth = new BankUser(bank, "10");
+        createBankUsers(bank);
 
-        Thread first = new Thread(bankUserFirst);
-        Thread second = new Thread(bankUserSecond);
-        Thread third = new Thread(bankUserThird);
-        Thread fourth = new Thread(bankUserFourth);
-        Thread fifth = new Thread(bankUserFifth);
-        Thread sixth = new Thread(bankUserSixth);
-        Thread seventh = new Thread(bankUserSeventh);
-        Thread eighth = new Thread(bankUserEighth);
-        Thread ninth = new Thread(bankUserNinth);
-        Thread tenth = new Thread(bankUserTenth);
+        createThreads(bankUserArrayList);
+
+        startThreadsBankUsers();
+
         /*
-        Блин... такой "ЛЯП" исправил...
-        > +        Thread sixth = new Thread(bankUserThird);
-        +        Thread seventh = new Thread(bankUserThird);
-        +        Thread eighth = new Thread(bankUserThird);
-        +        Thread ninth = new Thread(bankUserThird);
-        +        Thread tenth = new Thread(bankUserThird);
-         */
+        Выглядит так, словно все, что выше можно было сделать при помощи коллекции и цикла.
+        Лаконичная запись предпочтительнее, при условии сохранения легкости понимания логики.
 
-        first.start();
-        second.start();
-        third.start();
-        fourth.start();
-        fifth.start();
-        sixth.start();
-        seventh.start();
-        eighth.start();
-        ninth.start();
-        tenth.start();
+        Сделал
+         */
 
         /*
         +        while (first.isAlive() || second.isAlive()
@@ -77,5 +44,31 @@ public class MainBank {
         Остальные потоки продолжат свое "выполнение". Вы правы, цикл к данной ситуации попросту не
         нужен. Убрал.
         */
+    }
+
+    static void createBankUsers(Bank bank) {
+        /*
+        Блин... такой "ЛЯП" был, в прошлом моем ручном "создании" BankUsers
+        > +        Thread sixth = new Thread(bankUserThird);
+        +        Thread seventh = new Thread(bankUserThird);
+        +        Thread eighth = new Thread(bankUserThird);
+        +        Thread ninth = new Thread(bankUserThird);
+        +        Thread tenth = new Thread(bankUserThird);
+         */
+        for (int i = 1; i <= MAX_BANK_USERS; i++) {
+            bankUserArrayList.add(new BankUser(bank, ("BankUser #" + i)));
+        }
+    }
+
+    static void createThreads(ArrayList<BankUser> list) {
+        for (BankUser bankUser : list) {
+            threadsArrayList.add(new Thread(bankUser));
+        }
+    }
+
+    private static void startThreadsBankUsers() {
+        for (Thread thread : threadsArrayList) {
+            thread.start();
+        }
     }
 }
