9cc6ddeab0eb48f9d245180d33ec51cd8f0d65e2
Kirill <kirill_nn@inbox.ru>
2020-04-02 19:36:32 +0300

    Go to notebook

---
+++ collections/src/io/humb1t/ConcurrentLinkedQueueExampleTest.java
@@ -0,0 +1,13 @@
+package io.humb1t;
+
+public class ConcurrentLinkedQueueExampleTest {
+    /*
+    Не знаю насколько это хорошо, но один из FAQ говорит - разместите тест там же где тестируемый класс
+
+    How do I test protected methods?
+
+    Place your tests in the same package as the classes under test.
+
+    Refer to "Where should I put my test files?" for examples of how to organize tests for protected method access.
+     */
+}
